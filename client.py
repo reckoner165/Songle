2c07c552a3f19f2e4d118233a98a69b44618d812
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-12-23 18:09:45 -0500

    pushed effects module

--- client.py
+++ client.py
@@ -5,14 +5,20 @@ import struct
 
 import OSC, re, time, threading, math
 
+import effects
+import SoundModule as sm
+
 
 
 # GLOBAL VARIABLES FOR OSC TO CONTROL EFFECTS
-global gain
+# Default values
 gain = 0.5
+gain_robot = 0.0
+pan = 0.5
 
 # Socket for client streaming
 HOST = 'localhost'
+# HOST = '172.16.31.159'
 PORT = 9876
 ADDR = (HOST,PORT)
 BUFSIZE = 4096
@@ -21,8 +27,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 # OSC Setup
-osc_ra = '192.168.2.8', 7000 # Receiving Address (This computer)
-osc_sa = '192.168.2.5', 8080 # Sending Address (Phone with TouchOSC)
+osc_ra = '172.16.11.83', 7000 # Receiving Address (This computer)
+osc_sa = '172.17.105.77', 8080 # Sending Address (Phone with TouchOSC)
 
 osc_s = OSC.OSCServer(osc_ra)
 osc_c = OSC.OSCClient()
@@ -30,7 +36,7 @@ osc_c.connect(osc_sa)
 #
 osc_s.addDefaultHandlers()
 #
-# # DEFAULT HANDLER. Needs to be expanded with effects
+# # DEFAULT HANDLER.
 #
 def test_handler(addr, tags, stuff, source):
 	print "---"
@@ -45,24 +51,65 @@ def test_handler(addr, tags, stuff, source):
 	print "return message %s" % msg
 	print "---"
 
-def moveJoystick_handler(add, tags, stuff, source):
+
+# Handler to change Main Input gain. It responds to /1/fader1 in TouchOSC
+# It also returns the current gain value back to /1/memory
+def gain_handler(add, tags, stuff, source):
     global gain
     print "message received:"
     msg = OSC.OSCMessage()
-    msg.setAddress(addr)
+    msg.setAddress("/1/fader1")
     msg.append(stuff)
     osc_c.send(msg)
-    # print "X Value is: "
-    # print stuff[0]
-    gain = stuff[0]
-    # print "Y Value is: "
-    # print stuff[1]  #stuff is a 'list' variable
 
-osc_s.addMsgHandler("/1/fader1",moveJoystick_handler)
+    gain = stuff[0]
+
+    label_msg = OSC.OSCMessage()
+    label_msg.setAddress("/1/memory")
+    label_msg.append(gain)
+    osc_c.send(label_msg)
+
+# Handler to change Robotization gain. It responds to /1/fader2 in TouchOSC
+# It also returns the current robotization gain value back to /1/memory2
+def robot_handler(add, tags, stuff, source):
+    global gain_robot
+    print "message received:"
+    msg = OSC.OSCMessage()
+    msg.setAddress("/1/fader2")
+    msg.append(stuff)
+    osc_c.send(msg)
+
+    gain_robot = stuff[0]
+
+    label_msg = OSC.OSCMessage()
+    label_msg.setAddress("/1/memory2")
+    label_msg.append(gain_robot)
+    osc_c.send(label_msg)
+
+def pan_handler(add, tags, stuff, source):
+    global pan
+    print "message received:"
+    msg = OSC.OSCMessage()
+    msg.setAddress("/1/fader3")
+    msg.append(stuff)
+    osc_c.send(msg)
+
+    pan = stuff[0]
+
+    # label_msg = OSC.OSCMessage()
+    # label_msg.setAddress("/1/memory2")
+    # label_msg.append(gain_robot)
+    # osc_c.send(label_msg)
+
+# Patching OSC messages to respective handler functions
+osc_s.addMsgHandler("/1/fader1",gain_handler)
+osc_s.addMsgHandler("/1/fader2",robot_handler)
+osc_s.addMsgHandler("/1/fader3",pan_handler)
 
 for addr in osc_s.getOSCAddressSpace():
 	print addr
 
+# Starts a thread to look for OSC packets
 print "\nStarting OSCServer. Use ctrl-C to quit."
 st = threading.Thread( target = osc_s.serve_forever)
 st.start()
@@ -72,18 +119,12 @@ st.start()
 RATE = 44100
 p = pyaudio.PyAudio()
 
-# EFFECTS MODULES WILL BE RIGGED INTO A CHAIN HERE.
-
-
-# key = input('Enter key')
-
-# client.listen(5)
-
-
 while True:
     # data = client.recv(BUFSIZE)
     # print 'server connected ... ', addr
     # myfile = open('testfile.wav', 'w')
+
+    # Setup stream
     stream = p.open(format      = pyaudio.paInt16,
                 channels    = 2,
                 rate        = RATE,
@@ -94,22 +135,37 @@ while True:
 
 
     while True:
-        global gain
-        print gain
+        # Global variables for patching
+        global gain, gain_robot,pan
+        print gain, gain_robot,pan
         data = client.recv(BUFSIZE)
-        if not data: break
+        if not data:
+            print 'no data'
+            break
 
+        print 'receiving'
         input_tuple = struct.unpack('h'*2048, data)
         effects_in = input_tuple
-        # print type(effects_in)
-        effects_out = tuple([z * gain for z in effects_in])
-        # effects_out = effects_in
 
-        output = struct.pack('h'*2048, *effects_out)
+        # EFFECTS GO IN
+        eff1 = effects.robotization(effects_in, len(input_tuple))
 
+        robot = tuple([z * gain_robot for z in eff1])
+
+        effects_main = tuple([z * gain for z in effects_in])
+
+        effects_out = sm.mix(robot, effects_main)
+
+        effects_out = sm.clip(0.5,1,effects_out)
+
+        # OUTPUT TRACK WITH EFFECTS
+
+        # output = struct.pack('h'*2048, *effects_out)
+        output = sm.pan_stereo(effects_out,1-pan,pan)
         stream.write(output)
-        # print 'writing file ....'
+        print 'writing file ....'
 
+    # Graceful termination
     stream.close()
     print 'finished writing file'
     client.close()
---
+++ effects.py
@@ -0,0 +1,138 @@
+'''
+This file contains modules for different effects and tools to modilfy or filter 
+input stream of audio. This file is a part of DSP final project. 
+'''
+# 
+# Created by 
+# Drumil Mahajan, New York University, School of Engineering, 
+# Class of 2017. 
+
+import random
+import cmath
+import pyaudio
+import struct
+import wave
+import math
+import numpy as np
+	
+	
+def clip_n(input_data, n):
+	'''
+	This function takes two arguments.
+	stream is a floating point number
+	n is an integer
+
+	This funtions bound the value of input_data between max and min of n bit signed number
+	'''
+	
+	MAX = 2**(n-1) - 1
+	MIN = -2**(n)
+	if input_data > MAX:  
+		return MAX
+	elif input_data < MIN:
+		return MIN
+	else:
+		return input_data
+
+
+def robotization(input_tuple, BLOCKSIZE): 
+	'''
+	Parameters : 
+	BLOCKSIZE : size of the block, type int
+	input_string : input tuple of audio data of size BLOCKSIZE, type int
+	
+	Returns: 
+	output_block : list of output data. type int 
+	
+	Brief: 
+	Converts the input_string into robotized output_block
+	'''
+	
+
+	output_block = [0 for n in range(0, BLOCKSIZE)]
+
+	spect = np.fft.fft(input_tuple)
+	output = np.fft.ifft(np.absolute(spect))
+	for n in range(0,len(output)):
+		output_block[n] = clip_n(output[n], 16)
+	# output_block[n] = output[n]
+	#output_string = struct.pack('h' * 2 *  BLOCKSIZE, *output_block)
+	return output_block
+
+def whisperisation(input_tuple, BLOCKSIZE):
+	'''
+	Parameters :
+	BLOCKSIZE : size of the block, type int
+	input_string : input tuple of audio data of size BLOCKSIZE, type int
+
+	Returns:
+	output_block : list of output data. type int
+
+	Brief:
+	Converts the input_string into whisperization output_block
+	'''
+
+	output_block = [0 for n in range(0, BLOCKSIZE)]
+
+	random_phase = 2 * cmath.pi * random.random()
+	random_complex = cmath.cos(random_phase) + (cmath.sin(random_phase))*1j
+
+	spect = np.fft.fft(input_tuple)
+	output = np.fft.ifft(np.absolute(spect) * random_complex)
+
+	for n in range(0,len(output)):
+		output_block[n] = clip_n(output[n], 16)
+
+	return output_block
+	
+
+# def vibrato(BLOCKSIZE , f0 , W):
+#
+# 	# Create a buffer (delay line) for past values
+# 	buffer_MAX =  1024                          # Buffer length
+# 	buffer = [0.0 for i in range(buffer_MAX)]   # Initialize to zero
+#
+# 	# Buffer (delay line) indices
+# 	kr = 0  # read index
+# 	kw = int(0.5 * buffer_MAX)  # write index (initialize to middle of buffer)
+# 	kw = buffer_MAX/2
+#
+# 	# Get previous and next buffer values (since kr is fractional)
+#     kr_prev = int(math.floor(kr))
+#     kr_next = kr_prev + 1
+#     frac = kr - kr_prev    # 0 <= frac < 1
+#     if kr_next >= buffer_MAX:
+#         kr_next = kr_next - buffer_MAX
+#
+#     # Compute output value using interpolation
+#     output_value = (1-frac) * buffer[kr_prev] + frac * buffer[kr_next]
+#
+#     # Update buffer (pure delay)
+#     buffer[kw] = input_value
+#
+#     # Increment read index
+#     kr = kr + 1 + W * math.sin( 2 * math.pi * f0 * n / RATE )
+#         # Note: kr is fractional (not integer!)
+#
+#     # Ensure that 0 <= kr < buffer_MAX
+#     if kr >= buffer_MAX:
+#         # End of buffer. Circle back to front.
+#         kr = 0
+#
+#     # Increment write index
+#     kw = kw + 1
+#     if kw == buffer_MAX:
+#         # End of buffer. Circle back to front.
+#         kw = 0
+
+#
+# def butter_bandpass(data, lowcut, highcut, fs, order):
+# 	nyq = 0.5 * fs
+# 	low = lowcut / nyq
+# 	high = highcut / nyq
+# 	b, a = butter(order, [low, high], btype='band')
+# 	y = lfilter(b, a, data)
+# 	return y
+#
+	
+	
